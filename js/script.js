ecd3738bcdac4febb19aaae52e942a3cbbc19aa9
Patryk Komar <dsofiks@gmail.com>
2023-09-06 21:28:32 +0200

    readd

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-printMessage('Zagrałem ' + 'kamień' + '! Jeśli Twój ruch to , ' + 'papier ' + 'to wygrywasz!');
+
 var computerMove;
 computerMove = 'kamień';
 printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
